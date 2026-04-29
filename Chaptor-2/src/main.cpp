84818ea36e0959a58d91a0a93580891ae2f5c6e6
Kengokuma <kengo171227@gmail.com>
2026-04-29 22:19:35 +0900

    add Chaptor-2

---
+++ Chaptor-2/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(0);
+}
+
+void loop() {
+  myServo.write(0);
+  delay(1000);
+  myServo.write(90);
+  delay(1000);
+  myServo.write(180);
+  delay(1000);
+}
